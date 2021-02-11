e770369fa1e691d27c9f7a1426df895dfd45b1fb
suevalba <77942192+suevalba@users.noreply.github.com>
2021-02-11 01:32:13 -0800

    Updated CSS for styling on index.html

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-console.log ("Hello there! Thank you for viewing my site!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
