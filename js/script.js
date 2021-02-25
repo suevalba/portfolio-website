bc85152cea4ec14f0d7c015259cf2276592dbb10
suevalba <77942192+suevalba@users.noreply.github.com>
2021-02-25 00:15:52 -0800

    Created back to top button

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+
+  //Get the button:
+var topButton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
